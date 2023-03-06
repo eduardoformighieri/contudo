da6cd77a9852968061c991bc4cf039134aa27d16
Eduardo Silva <eduardolima1998@gmail.com>
2023-03-05 22:43:11 -0300

    fix: auth service

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,22 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AdminsService } from '../admins/admins.service';
 import { JwtService } from '@nestjs/jwt';
 import { ValidateAdminDto } from './dto/validate-admin.dto';
 import { Admin, Prisma } from '@prisma/client';
 
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly adminsService: AdminsService,
     private readonly jwtService: JwtService,
+    private prisma: PrismaService,
   ) {}
 
+  async findOne(
+    postWhereUniqueInput: Prisma.AdminWhereUniqueInput,
+  ): Promise<Admin> {
+    const admin = await this.prisma.admin.findUnique({
+      where: postWhereUniqueInput,
+    });
+
+    if (!admin) {
+      throw new NotFoundException('Admin not found');
+    }
+
+    return admin;
+  }
+
   async validateAdmin({ email, password }: ValidateAdminDto): Promise<any> {
-    const user = await this.adminsService.findOne({ email });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const { password, ...result } = user;
+    const admin = await this.findOne({ email });
+    if (admin && (await bcrypt.compare(password, admin.password))) {
+      const { password, ...result } = admin;
       return result;
     }
     return null;
