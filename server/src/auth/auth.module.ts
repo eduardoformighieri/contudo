fa15e1ed3f0c79b8d44c40d9214db6c96085df8d
Eduardo Silva <eduardolima1998@gmail.com>
2023-03-17 17:47:19 -0300

    feat: change and hide jwt expiration

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy, PrismaService],
