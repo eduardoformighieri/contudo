597d411a4373f88ce2ae4c88374817d038d2aee9
Eduardo Silva <eduardolima1998@gmail.com>
2023-04-26 03:16:43 -0300

    fix: remove double BrowserRouter

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,13 +5,11 @@ import { GuestRoutes } from './Guest';
 
 export const RootRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/admin/*" element={<AdminRoutes />} />
-        <Route path="/guest/*" element={<GuestRoutes />} />
-        <Route path="*" element={<Navigate to="/auth" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/auth/*" element={<AuthRoutes />} />
+      <Route path="/admin/*" element={<AdminRoutes />} />
+      <Route path="/guest/*" element={<GuestRoutes />} />
+      <Route path="*" element={<Navigate to="/auth" replace />} />
+    </Routes>
   );
 };
